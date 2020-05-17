faeb64c9d502300342dec1694e92aa6f681a5343
Mustafa Khairullah <mustafa.khairullah@gmail.com>
2020-05-17 16:13:35 -0600

    Adding a new view

--- polls/views.py
+++ polls/views.py
@@ -39,3 +39,6 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def chart(request):
+    return render(request, 'polls/chart.html')
